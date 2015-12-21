d0a493a870f1811ac132283f229ef4ef3a9870d9
Ahmed Galal <ahmedgalal007@hotmail.com>
2015-12-21 19:17:52 +0200

    Show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,15 @@
 var socket = io();
+var now = moment();
 
 socket.on('connect', function () {
 	console.log('Connected to socket.io server! ');
 });
 
 socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('New Message:');
 	console.log(message.text);
-	jQuery('.messages').append('<p>' + message.text + '</p>'); 	
+	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a ') + ': </strong>' + message.text + '</p>'); 	
 });
 
 // Handels submitting of new message
@@ -18,7 +20,8 @@ $form.on('submit', function (event) {
 	event.preventDefault();
 	var $message = $form.find('input[name=message]');
 	socket.emit('message', {
-		text: $message.val()
+		text: $message.val(),
+		//timestamp: now.format('h:mm a')
 	});
 
 	$message.val('');
--- server.js
+++ server.js
@@ -3,19 +3,24 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var moment = require('moment');
+var now = moment();
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
 
 	socket.on('message', function (message){
-		console.log('Message recieved: ' + message.text );
-		//socket.broadcast.emit('message', message);
+		
+		console.log('Message recieved: ' + message.text + ' at: ' + message.timestamp );
+		
+		message.timestamp = moment().valueOf();
 		io.emit('message', message);
 	})
 	socket.emit('message', {
-		text: "Welcome to the chat application!"
+		text: "Welcome to the chat application!",
+		//moment.utc(now.format('x')).local()
+		timestamp: moment().valueOf()
 	});
 
 });
